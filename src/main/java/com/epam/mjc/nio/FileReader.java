8abf327c688a6bb0ad45be3ffca83248b8962497
SarvarKhalimov <khalimovsarvar@gmail.com>
2022-09-25 01:22:35 +0500

    Solve the task with NIO

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,52 @@
 package com.epam.mjc.nio;
-
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
+    public static void main(String[] args) {
+        FileReader fileReader = new FileReader();
+        File file = new File("src/main/resources/Profile.txt");
+        fileReader.getDataFromFile(file);
+    }
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder bld = new StringBuilder();
+
+        try (RandomAccessFile aFile = new RandomAccessFile(file.getPath(), "r");
+             FileChannel inChannel = aFile.getChannel();) {
+
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+            while (inChannel.read(buffer) > 0) {
+                buffer.flip();
+                for (int i = 0; i < buffer.limit(); i++) {
+                    bld.append(Character.toString((char) buffer.get()));
+                }
+                buffer.clear(); // do something with the data and clear/compact it.
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String stringContentOfFile = bld.toString();
+
+        String[] parsedArg = new String[4];
+        String[] keyVals = stringContentOfFile.split("\n");
+        for (int i = 0; i < keyVals.length; i++) {
+            String[] parts = keyVals[i].split(":",2);
+            parsedArg[i] = parts[1].trim();
+        }
+
+        Profile profile = new Profile(
+                parsedArg[0],
+                Integer.valueOf(parsedArg[1]),
+                parsedArg[2],
+                Long.parseLong(parsedArg[3]));
+        System.out.println(profile);
+        return profile;
     }
 }
